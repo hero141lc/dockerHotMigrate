75708d22961ac6c0472043372238e1683c1f61d8
yylxc <11085585+yylxc@user.noreply.gitee.com>
2023-03-22 21:32:48 +0800

    ss

--- main.py
+++ main.py
@@ -63,6 +63,7 @@ def sendData():
 def reliveDocker():
     os.system(r"docker start --checkpoint-dir=./tempData/ --checkpoint=checkpoint2 looper2")
 def clearDocker():
+    os.system(r"docker checkpoint rm looper2 checkpoint2")
     os.system(r"docker stop looper2")
     os.system(r"docker rm looper2")
     os.system("rm -rf tempData")
